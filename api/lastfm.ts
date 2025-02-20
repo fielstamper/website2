330044a77d945e948438eaa9146877d9740702e3
caitlyn <161728157+fielstamper@users.noreply.github.com>
2025-02-21 06:28:15 +0800

    dadada

--- api/lastfm.ts
+++ api/lastfm.ts
@@ -3,33 +3,26 @@ let cachedResponse: object | undefined;
 
 export async function GET(request: Request) {
     const now = Date.now();
-    const waitMs: number = 15 * 1000;
 
-    if (!cachedResponse) {
-        // If there's no cached response, fetch it immediately
-        cachedResponse = await fetchRecentTrack();
+    const waitMs: number = 15 * 1000;
+    if (!cachedResponse || now - lastUpdate >= waitMs) {
         lastUpdate = now;
-    } else if (now - lastUpdate >= waitMs) {
-        // If cache is expired, trigger async update but return immediately
-        lastUpdate = now;
-        fetchRecentTrack().then(data => cachedResponse = data);
+
+        const params = new URLSearchParams({
+            method: "user.getrecenttracks",
+            limit: "1",
+            user: "swagster736",
+            api_key: "f882cdfc41488cde6404236b20bb0095",
+            format: "json"
+        });
+
+        const url = `https://ws.audioscrobbler.com/2.0/?${params.toString()}`;
+        const response = await fetch(url);
+        // TODO: Check for errors.
+        const data = await response.json();
+        cachedResponse = data.recenttracks.track[0];
+
     }
 
     return Response.json(cachedResponse);
-}
-
-async function fetchRecentTrack() {
-    const params = new URLSearchParams({
-        method: "user.getrecenttracks",
-        limit: "1",
-        user: "jopenuts",
-        api_key: "  12312321 3213 21 3213 ",
-        format: "json"
-    });
-
-    const url = `https://ws.audioscrobbler.com/2.0/?${params.toString()}`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return data.recenttracks.track[0];
 }
