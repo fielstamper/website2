ff7ce371684375f296d4e9ea5d57a13cff7202bd
caitlyn <161728157+fielstamper@users.noreply.github.com>
2025-02-21 07:19:06 +0800

    adaada

--- api/lastfm.ts
+++ api/lastfm.ts
@@ -3,22 +3,68 @@ let cachedResponse: object | undefined;
 
 export async function GET(request: Request) {
     const now = Date.now();
-    const waitMs: number = 15 * 1000;
+    const waitMs: number = 10 * 1000;
 
     if (!cachedResponse) {
         // If there's no cached response, fetch it immediately
-        cachedResponse = await fetchRecentTrack();
+        const data = await fetchRecentTrack();
+
+        cachedResponse = {
+            image: data['image'][2]['#text'],
+            data: data
+        }
+
+        imageToBase64(data['image'][2]['#text']).then(base64 => {
+            if (base64) {
+                cachedResponse = {
+                    image: base64,
+                    data: data
+                }
+            }
+        });
         lastUpdate = now;
     } else if (now - lastUpdate >= waitMs) {
         // If cache is expired, trigger async update but return immediately
         lastUpdate = now;
-        fetchRecentTrack().then(data => cachedResponse = data);
+        fetchRecentTrack().then(response => {
+            if (cachedResponse!['data']['name'] !== response['name']) {
+                const data = response['data']
+                cachedResponse = {
+                    image: data['image'][2]['#text'],
+                    data: data
+                }
+
+                imageToBase64(data['image'][2]['#text']).then(base64 => {
+                    if (base64) {
+                        cachedResponse = {
+                            image: base64,
+                            data: data
+                        }
+                    }
+                });
+            }
+        });
     }
 
     return Response.json(cachedResponse);
 }
 
-async function fetchRecentTrack() {
+async function imageToBase64(imageUrl: string): Promise<string | null> {
+    try {
+        const response = await fetch(imageUrl);
+        const arrayBuffer = await response.arrayBuffer();
+        const base64String = Buffer.from(arrayBuffer).toString("base64");
+        const mimeType = response.headers.get("content-type") || "image/png";
+
+        return `data:${mimeType};base64,${base64String}`;
+    } catch (error) {
+        console.error("Error converting image to Base64:", error);
+        return null;
+    }
+}
+
+
+async function fetchRecentTrack(): Promise<object> {
     const params = new URLSearchParams({
         method: "user.getrecenttracks",
         limit: "1",
